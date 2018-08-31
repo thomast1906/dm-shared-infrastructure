0f613694ade27202aed7c3480b26ad6df98a3876
Dwayne Bailey <Dwayne.Bailey@HMCTS.net>
2018-08-31 16:11:47 +0100

    Cleanup: s/rg/shared_rg/ for readability

--- key-vault.tf
+++ key-vault.tf
@@ -5,7 +5,7 @@ module "shared-vault" {
   env = "${var.env}"
   tenant_id = "${var.tenant_id}"
   object_id = "${var.jenkins_AAD_objectId}"
-  resource_group_name = "${azurerm_resource_group.rg.name}"
+  resource_group_name = "${azurerm_resource_group.shared_rg.name}"
   product_group_object_id = "be8b3850-998a-4a66-8578-da268b8abd6b"
 }
 
