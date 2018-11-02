093f9ad4a1910aee0182a28a023e38716a5e57d7
Dwayne Bailey <Dwayne.Bailey@HMCTS.net>
2018-11-02 10:43:18 +0000

    Tag infra with common_tags
    RDM-3282

--- main.tf
+++ main.tf
@@ -1,4 +1,16 @@
+locals {
+  tags = "${merge(
+    var.common_tags,
+    map(
+      "Team Contact", var.team_contact,
+      "Destroy Me", var.destroy_me
+    )
+  )}"
+}
+
 resource "azurerm_resource_group" "shared_rg" {
   name     = "${var.product}-shared-${var.env}"
   location = "${var.location}"
+
+  tags = "${local.tags}"
 }
--- storage-account.tf
+++ storage-account.tf
@@ -13,7 +13,7 @@ module "storage_account" {
   enable_https_traffic_only = true
 
   // Tags
-  team_name    = "${var.team_name}"
+  common_tags  = "${local.tags}"
   team_contact = "${var.team_contact}"
   destroy_me   = "${var.destroy_me}"
 }
