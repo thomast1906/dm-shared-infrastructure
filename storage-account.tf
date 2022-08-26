ef062a8fc66b78c0355a5d972925544cb4cb5726
yogesh-hullatti <yogesh.hullatti@hmcts.net>
2022-08-26 09:39:16 +0100

    Fixed build error

--- storage-account.tf
+++ storage-account.tf
@@ -34,20 +34,20 @@ provider "azurerm" {
 }
 
 data "azurerm_virtual_network" "aks_core_vnet" {
-  provider             = "azurerm.aks-infra"
+  provider             = azurerm.aks-infra
   name                 = "core-${var.env}-vnet"
   resource_group_name  = "aks-infra-${var.env}-rg"
 }
 
 data "azurerm_subnet" "aks-00" {
-  provider             = "azurerm.aks-infra"
+  provider             = azurerm.aks-infra
   name                 = "aks-00"
   virtual_network_name = data.azurerm_virtual_network.aks_core_vnet.name
   resource_group_name  = data.azurerm_virtual_network.aks_core_vnet.resource_group_name
 }
 
 data "azurerm_subnet" "aks-01" {
-  provider             = "azurerm.aks-infra"
+  provider             = azurerm.aks-infra
   name                 = "aks-01"
   virtual_network_name = data.azurerm_virtual_network.aks_core_vnet.name
   resource_group_name  = data.azurerm_virtual_network.aks_core_vnet.resource_group_name
